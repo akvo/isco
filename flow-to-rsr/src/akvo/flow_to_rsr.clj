3215e858c5375551ab99128a7c7bad8cc957e619
Juan A. Ruz <juanantonioruz@gmail.com>
2021-03-04 10:33:05 +0100

    Refactor

--- flow-to-rsr/src/akvo/flow_to_rsr.clj
+++ flow-to-rsr/src/akvo/flow_to_rsr.clj
@@ -1,61 +1,33 @@
 (ns akvo.flow-to-rsr
-  (:require [akvo.lumen.lib.import.common :as common]
-            [akvo.lumen.postgres :as postgres]
-            [akvo.rsr-api :as rsr-api]
-            [cheshire.core :as cheshire]
-            [akvo.lumen.protocols :as p]
-            [akvo.lumen.component.flow :as c.flow]
-            [clojure.tools.logging :as log]))
+  (:require
+   [akvo.rsr-api :as rsr-api]
+   [akvo.flow :as flow]
+   [akvo.rsr :as rsr]
+   [cheshire.core :as cheshire]
+   [clojure.tools.logging :as log]))
 
-(defn adapt-columns [importer-columns]
-  (map (fn [c] (-> c
-                   (update :groupName (fn [groupName] (or groupName "main")))
-                   (update :groupId (fn [groupId] (or groupId "main")))))
-       importer-columns))
+(comment "flow part"
+  (let [jwt-token dev/jwt]
+    (flow/execute (flow/source "uat1" "722939140" "713029131" "juan@akvo.org" jwt-token)
+             (flow/import-config "https://api-auth0.akvotest.org/flow"))))
 
-(defn import-config [flow-auth0-api-url]
-  {:flow-api {:url flow-auth0-api-url
-              :internal-url flow-auth0-api-url
-              :api-headers akvo.lumen.component.flow/api-headers
-              :internal-api-headers akvo.lumen.component.flow/api-headers}
-   :environment {"data-groups" true}})
-
-(defn source [instance survey-id form-id email token]
-  {"kind" "AKVO_FLOW"
-   "instance" "uat1"
-   "surveyId" survey-id
-   "formId" form-id
-   "version" 3
-   "email" email
-   "token" token})
-
-(defn execute [source import-config]
-  (with-open [importer (common/datagroups-importer source import-config)]
-    (let [rows (p/records importer)
-          columns  (adapt-columns (p/columns importer))]
-      (doseq [[groupId cols] (group-by :groupId columns)]
-        (log/error groupId cols))
-      (doseq [response (take common/rows-limit rows)]
-        (doseq [[groupId iterations] response]
-          (log/error (mapv postgres/coerce-to-sql iterations)))))))
-
-(comment
-  (let [jwt-token "your-jwt-token"]
-    (execute (source "uat1" "722939140" "713029131" "juan@akvo.org" jwt-token)
-             (import-config "https://api-auth0.akvotest.org/flow"))))
+(comment "rsr part"
+  (rsr/me dev/rsr-token)
+  (let [rsr-api-token dev/rsr-token]
+    (rsr/project-indicators dev/rsr-token 9269)
+    #_(let [id 32771]
+        (log/error :current-value-for id
+                   (-> (rsr-api/read-indicator-period-data-framework rsr-api-token id)
+                       :body
+                       :value)))
+    #_(let [user-id 45824
+            period-id 435305
+            value (+ 10 (rand-int 100))]
+        (log/error :writing-value
+                   (rsr-api/write-indicator-period-data rsr-api-token user-id period-id value)))
+    )
 
 
 
-(comment
-  (let [rsr-api-token "rsr-api-token"]
-    (let [id 32771]
-      (log/error :current-value-for id
-                 (-> (rsr-api/read-indicator-period-data-framework rsr-api-token id)
-                     :body
-                     :value)))
-    (let [user-id 45824
-          period-id 435305
-          value (+ 10 (rand-int 100))]
-      (log/error :writing-value
-                 (rsr-api/write-indicator-period-data rsr-api-token user-id period-id value)))
-    ))
+  (def mapping {"c732649139" 121258
+                "c739539130" 121257}))
