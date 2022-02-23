e22e4b80c5461e5ce7d1e850f150c79c25e280ff
wayangalihpratama <wgprtm@gmail.com>
2022-02-23 16:04:10 +0800

    [#19] Create user account endpoint

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,23 +1,29 @@
 from middleware import Token, authenticate_user
-from middleware import create_access_token
+from middleware import create_access_token, verify_user
 from middleware import ACCESS_TOKEN_EXPIRE_MINUTES
 from fastapi import Depends, HTTPException, status, APIRouter, Request
+from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBasicCredentials as credentials
 from sqlalchemy.orm import Session
 from db.connection import get_session
 from datetime import timedelta
+from models.user import UserDict
+from pydantic import SecretStr
 
+security = HTTPBearer()
 user_route = APIRouter()
 
 
-@user_route.post("/login",
+@user_route.post("/user/login",
                  response_model=Token,
                  summary="user login",
+                 name="user:login",
                  tags=["User"])
-def login(req: Request, email: str, password: str,
+def login(req: Request, email: str, password: SecretStr,
           session: Session = Depends(get_session)):
     user = authenticate_user(session=session,
                              email=email,
-                             password=password)
+                             password=password.get_secret_value())
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -25,7 +31,17 @@ def login(req: Request, email: str, password: str,
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.email},
+                                       expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@user_route.get("/user/me",
+                response_model=UserDict,
+                summary="get account information",
+                name="user:me",
+                tags=["User"])
+def me(req: Request, session: Session = Depends(get_session),
+       credentials: credentials = Depends(security)):
+    user = verify_user(session=session, authenticated=req.state.authenticated)
+    return user
