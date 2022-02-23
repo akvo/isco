9b200b5a4a117e76ae0fea37f1cf7c3ac8560262
wayangalihpratama <wgprtm@gmail.com>
2022-02-23 16:02:37 +0800

    [#19] Add route middleware

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes.organisation import organisation_route
 from routes.user import user_route
+from middleware import decode_token
 
 app = FastAPI(
     root_path="/api",
@@ -29,3 +30,13 @@ def read_main():
 @app.get("/health-check", tags=["Dev"])
 def health_check():
     return "OK"
+
+
+@app.middleware("http")
+async def route_middleware(request: Request, call_next):
+    auth = request.headers.get('Authorization')
+    if auth:
+        auth = decode_token(auth.replace("Bearer ", ""))
+        request.state.authenticated = auth
+    response = await call_next(request)
+    return response
