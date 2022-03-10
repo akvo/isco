c117ca8fa474c7eead0dbacbe7664d142f0f6be0
wayangalihpratama <wgprtm@gmail.com>
2022-03-10 17:48:08 +0800

    [#34] Create admin landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components";
-import { Home } from "./pages";
+import { Home, Admin } from "./pages";
 
 const App = () => {
   return (
@@ -12,8 +12,10 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
+          <Route exact path="/admin" element={<Admin />} />
         </Routes>
       </Layout.Body>
+      <Layout.Footer />
     </Layout>
   );
 };
---
+++ frontend/src/pages/admin/Admin.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.scss";
+import { Row, Col, Typography, Card, Button } from "antd";
+
+const { Title } = Typography;
+
+const loremText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eleifend felis eu tempor porttitor. Suspendisse sit amet nunc nec neque tempus laoreet nec ut velit. Vivamus eleifend purus at iaculis consequat. Cras maximus facilisis elit at vehicula. Proin viverra ipsum egestas scelerisque mattis. Vivamus vel ex quis augue commodo bibendum nec et velit. Curabitur ut lorem sed purus varius tristique. Cras eu metus tincidunt, convallis nisl eget, dapibus risus. Cras semper ullamcorper magna, ut mattis velit semper in. Vestibulum finibus nunc pharetra lorem facilisis suscipit.";
+
+const Admin = () => {
+  return (
+    <div id="admin">
+      <Row className="container bg-grey">
+        <Col span={24}>
+          <Title className="page-title" level={3}>
+            Welcome Admin
+          </Title>
+          <Row className="card-row" gutter={[20, 20]}>
+            <Col md={24} lg={12}>
+              <Card
+                className="card-wrapper"
+                title={<Title level={5}>Manage Users</Title>}
+              >
+                <p>{loremText}</p>
+                <div className="card-footer">
+                  <Button type="primary" ghost href="#">
+                    Manage Users
+                  </Button>
+                </div>
+              </Card>
+            </Col>
+            <Col md={24} lg={12}>
+              <Card
+                title={<Title level={5}>Manage Surveys</Title>}
+                className="card-wrapper"
+              >
+                <p>{loremText}</p>
+                <div className="card-footer">
+                  <Button type="primary" ghost href="#">
+                    Manage Surveys
+                  </Button>
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Admin;
