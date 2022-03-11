d2637d985d7d76035ae5f59b797b02764e317560
wayangalihpratama <wgprtm@gmail.com>
2022-03-11 16:19:42 +0800

    [#35] Set Question input text as a panel collapsible

--- frontend/src/components/survey-editor/QuestionEditor.jsx
+++ frontend/src/components/survey-editor/QuestionEditor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Row,
   Col,
@@ -10,11 +10,15 @@ import {
   Select,
   Collapse,
 } from "antd";
-import { RiQuestionFill } from "react-icons/ri";
+import { RiQuestionFill, RiSettings5Fill } from "react-icons/ri";
+import { BiRadioCircle } from "react-icons/bi";
 
 const { Panel } = Collapse;
 
 const Question = ({ form }) => {
+  const [activePanel, setActivePanel] = useState(null);
+  console.log(activePanel);
+
   return (
     <Row className="question-editor-wrapper">
       <Col span={24}>
@@ -26,10 +30,46 @@ const Question = ({ form }) => {
             gutter={[12, 12]}
           >
             <Col span={18} align="start" className="left">
-              <Form.Item label="Q1" name="question-group-name">
-                <Input placeholder="Section Title" />
-              </Form.Item>
+              <Collapse ghost activeKey={activePanel}>
+                <Panel
+                  key="q1"
+                  showArrow={false}
+                  header={
+                    <Form.Item
+                      label={
+                        <Button
+                          type="text"
+                          size="small"
+                          onClick={() =>
+                            setActivePanel(activePanel ? null : "q1")
+                          }
+                        >
+                          Q1
+                        </Button>
+                      }
+                      name="question-group-name"
+                    >
+                      <Input placeholder="Enter your question" />
+                    </Form.Item>
+                  }
+                >
+                  <Row className="panel-body-wrapper">
+                    <Col className="button-wrapper" span={1}>
+                      <Button type="text" icon={<RiSettings5Fill />} />
+                    </Col>
+                    <Col className="input-wrapper" span={23}>
+                      <Form.Item label={<BiRadioCircle />} name="option">
+                        <Input placeholder="Enter an answer choice" />
+                      </Form.Item>
+                      <Form.Item label={<BiRadioCircle />} name="option">
+                        <Input placeholder="Enter an answer choice" />
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Panel>
+              </Collapse>
             </Col>
+
             <Col span={6} align="end" className="right">
               <Space align="center">
                 <Select
