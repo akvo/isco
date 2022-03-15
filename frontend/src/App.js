a281d2039d0d60259368a17ab649bff39ace65cf
wayangalihpratama <wgprtm@gmail.com>
2022-03-15 14:21:26 +0800

    [#40] Add fetch to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import "./App.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components";
 import { Home, Admin, ManageSurvey, ManageUser, SurveyEditor } from "./pages";
+import { store, api } from "./lib";
 
 const App = () => {
+  useEffect(() => {
+    Promise.all([
+      api.get("question/type"),
+      api.get("member_type/"),
+      api.get("isco_type/"),
+    ]).then((res) => {
+      const [question_type, member_type, isco_type] = res;
+      store.update((s) => {
+        s.optionValues = {
+          ...s.optionValues,
+          question_type: question_type?.data,
+          member_type: member_type?.data,
+          isco_type: isco_type?.data,
+        };
+      });
+    });
+  }, []);
+
   return (
     <Layout>
       <Layout.Header />
