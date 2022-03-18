98cca518dfc23d51efa6e39e8a111ff3a3ae2500
dedenbangkit <mail@dedenbangkit.com>
2022-03-18 09:48:27 +0700

    [#43] Clean CI Database Debug

--- backend/run.sh
+++ backend/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2001
 
-DB_HOST=$(sed 's/^.*\(@.*:\).*$/\1/' <<< "${DATABASE_URL}" | sed 's/@//' | sed 's/\://')
-echo "DATABASE HOST: ${DB_HOST}"
-./wait-for-it.sh -h "${DB_HOST}" -p 5432 -- echo "Database is up and running"
-
 if [[ -z "${SKIP_MIGRATION}" ]]; then
     alembic upgrade head
 fi
