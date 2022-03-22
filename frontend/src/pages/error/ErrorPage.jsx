18d5650e232b5e49af67466b6c628513c6764c69
wayangalihpratama <wgprtm@gmail.com>
2022-03-22 13:45:58 +0800

    [#48] Create error page

---
+++ frontend/src/pages/error/ErrorPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import { Result, Button } from "antd";
+
+const ErrorPage = ({ status = 500 }) => {
+  const navigate = useNavigate();
+
+  const BackHome = () => (
+    <Button type="primary" ghost onClick={() => navigate("/home")}>
+      Back Home
+    </Button>
+  );
+
+  const config = () => {
+    switch (status) {
+      case 403:
+        return {
+          status: status,
+          title: "Not authorized.",
+          subTitle: "Sorry, you are not authorized to access this page.",
+          extra: <BackHome />,
+        };
+      case 404:
+        return {
+          status: status,
+          title: "Page not found.",
+          subTitle: "Sorry, the page you visited does not exist.",
+          extra: <BackHome />,
+        };
+      default:
+        return {
+          status: status,
+          title: "Error",
+          subTitle: "Sorry, something went wrong.",
+          extra: <BackHome />,
+        };
+    }
+  };
+
+  return (
+    <div id="error">
+      <Result {...config()} />
+    </div>
+  );
+};
+
+export default ErrorPage;
