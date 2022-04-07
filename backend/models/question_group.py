87aa22c781a9a3820bb6f667c4891888331b4531
wayangalihpratama <wgprtm@gmail.com>
2022-04-07 11:30:30 +0800

    [#80] Add repeat text to payload & question group model

--- backend/models/question_group.py
+++ backend/models/question_group.py
@@ -21,6 +21,7 @@ class QuestionGroupPayload(TypedDict):
     translations: Optional[List[dict]] = None
     order: Optional[int] = None
     repeat: Optional[bool] = None
+    repeat_text: Optional[str] = None
     member_access: Optional[List[int]] = None
     isco_access: Optional[List[int]] = None
     question: Optional[List[QuestionPayload]] = None
@@ -34,6 +35,7 @@ class QuestionGroupDict(TypedDict):
     translations: Optional[List[dict]] = []
     order: Optional[int] = None
     repeat: bool
+    repeat_text: Optional[str] = None
 
 
 class QuestionGroup(Base):
@@ -44,6 +46,7 @@ class QuestionGroup(Base):
     description = Column(Text, nullable=True)
     translations = Column(pg.ARRAY(pg.JSONB), nullable=True)
     repeat = Column(Boolean, default=False)
+    repeat_text = Column(String, nullable=True)
     order = Column(Integer, nullable=True)
     created = Column(DateTime, default=datetime.utcnow)
     member_access = relationship(
@@ -69,7 +72,7 @@ class QuestionGroup(Base):
     def __init__(self, id: Optional[int], form: int, name: str,
                  translations: Optional[List[dict]],
                  repeat: Optional[bool], order: Optional[int],
-                 description: Optional[str]):
+                 description: Optional[str], repeat_text: Optional[str]):
         self.id = id
         self.form = form
         self.name = name
@@ -77,6 +80,7 @@ class QuestionGroup(Base):
         self.translations = translations
         self.order = order
         self.repeat = repeat
+        self.repeat_text = repeat_text
 
     def __repr__(self) -> int:
         return f"<QuestionGroup {self.id}>"
@@ -95,6 +99,7 @@ class QuestionGroup(Base):
             "translations": translations,
             "order": self.order,
             "repeat": self.repeat,
+            "repeat_text": self.repeat_text,
             "member_access": [ma.member_type for ma in self.member_access],
             "isco_access": [ia.isco_type for ia in self.isco_access],
             "question": [q.serialize for q in self.question]
@@ -113,8 +118,19 @@ class QuestionGroup(Base):
         }
         if self.repeat:
             group.update({"repeatButtonPlacement": "bottom"})
+        if self.repeat and self.repeat_text:
+            group.update({"repeatText": self.repeat_text})
         if self.translations:
-            group.update({"translations": self.translations})
+            translations = []
+            for lang in self.translations:
+                tmp = {
+                    "name": lang['name'],
+                    "description": lang['description']
+                }
+                if self.repeat and self.repeat_text:
+                    tmp.update({"repeatText": lang['repeat_text']})
+                translations.append(tmp)
+            group.update({"translations": translations})
         return group
 
 
@@ -126,6 +142,7 @@ class QuestionGroupBase(BaseModel):
     translations: Optional[List[dict]] = []
     order: Optional[int] = None
     repeat: bool
+    repeat_text: Optional[str] = None
     member_access: Optional[List[int]] = []
     isco_access: Optional[List[int]] = []
     question: Optional[List[QuestionBase]] = []
@@ -140,6 +157,7 @@ class QuestionGroupJson(BaseModel):
     translations: Optional[List[dict]] = []
     order: Optional[int] = None
     repeatable: bool
+    repeatText: Optional[str] = None
     member_access: Optional[List[str]] = []
     isco_access: Optional[List[str]] = []
     question: Optional[List[dict]] = []
