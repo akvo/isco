c4bf3abac27ae8c536e2fa4ad48785b0739676cd
wayangalihpratama <wgprtm@gmail.com>
2022-04-08 20:23:33 +0800

    [#74] Create test case for move group & question

---
+++ backend/tests/test_080_move.py
@@ -0,0 +1,83 @@
+import sys
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from sqlalchemy.orm import Session
+from datetime import datetime
+from tests.test_000_main import Acc
+
+pytestmark = pytest.mark.asyncio
+sys.path.append("..")
+
+account = Acc(email=None, token=None)
+
+
+def datenow():
+    now = datetime.utcnow()
+    return now.strftime("%d-%m-%Y")
+
+
+class TestMoveGroupAndQuestionGroup():
+    @pytest.mark.asyncio
+    async def test_move_question_group(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.put(
+            app.url_path_for("question_group:move",
+                             id=1,
+                             selected_order=1,
+                             target_order=2),
+            params={"target_id": 2},
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 204
+
+    @pytest.mark.asyncio
+    async def test_move_question_group_back(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.put(
+            app.url_path_for("question_group:move",
+                             id=1,
+                             selected_order=2,
+                             target_order=1),
+            params={"target_id": 1},
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 204
+
+    @pytest.mark.asyncio
+    async def test_move_question(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.put(
+            app.url_path_for("question:move",
+                             id=1,
+                             selected_order=1,
+                             target_order=2),
+            params={"target_group": 1},
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 204
+
+    @pytest.mark.asyncio
+    async def test_move_question_back(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.put(
+            app.url_path_for("question:move",
+                             id=1,
+                             selected_order=2,
+                             target_order=1),
+            params={"target_group": 1},
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 204
---
+++ backend/tests/test_090_load_data.py
@@ -0,0 +1,87 @@
+import sys
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from sqlalchemy.orm import Session
+from datetime import datetime
+from tests.test_000_main import Acc
+
+pytestmark = pytest.mark.asyncio
+sys.path.append("..")
+
+account = Acc(email=None, token=None)
+
+
+def datenow():
+    now = datetime.utcnow()
+    return now.strftime("%d-%m-%Y")
+
+
+class TestLoadData():
+    @pytest.mark.asyncio
+    async def test_get_all_organisation(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.get(app.url_path_for("organisation:get_all"))
+        assert res.status_code == 200
+        res = res.json()
+        assert res[0] == {
+            'active': True,
+            'code': 'Akvo',
+            'id': 1,
+            'isco': ['All'],
+            'isco_type': [1],
+            'member': 'All',
+            'member_type': 1,
+            'name': 'Akvo'
+        }
+
+    @pytest.mark.asyncio
+    async def test_get_all_user(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.get(
+            app.url_path_for("user:get_all"),
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 200
+        res = res.json()
+        assert "current" in res
+        assert "data" in res
+        assert "total" in res
+        assert "total_page" in res
+        assert "email" in res["data"][0]
+        assert "email_verified" in res["data"][0]
+        assert "id" in res["data"][0]
+        assert "name" in res["data"][0]
+        assert "organisation" in res["data"][0]
+        assert "role" in res["data"][0]
+
+    @pytest.mark.asyncio
+    async def test_get_all_member(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.get(app.url_path_for("member_type:get_all"))
+        assert res.status_code == 200
+        res = res.json()
+        assert res[0] == {"id": 1, "name": "All"}
+
+    @pytest.mark.asyncio
+    async def test_get_all_isco(
+        self,
+        app: FastAPI,
+        session: Session,
+        client: AsyncClient
+    ) -> None:
+        res = await client.get(app.url_path_for("isco_type:get_all"))
+        assert res.status_code == 200
+        res = res.json()
+        assert res[0] == {"id": 1, "name": "All"}
