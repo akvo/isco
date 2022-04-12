ae234818a790d11cba193c24e7ec646f45dbf0be
wayangalihpratama <wgprtm@gmail.com>
2022-04-12 13:09:14 +0800

    [#115] Homepage translation

--- frontend/src/static/index.js
+++ frontend/src/static/index.js
@@ -2,3 +2,4 @@ export { default as definitionContent } from "./definition-content";
 export { default as impressumContent } from "./impressum-content";
 export { default as dataSecurityContent } from "./data-security-content";
 export { default as uiText } from "./ui-text";
+export { default as homeContent } from "./home-content";
