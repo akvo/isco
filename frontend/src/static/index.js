be4c3029537997392765bb5fe62333790fa300eb
wayangalihpratama <wgprtm@gmail.com>
2022-04-12 12:49:15 +0800

    [#115] Add uiText translation

--- frontend/src/static/index.js
+++ frontend/src/static/index.js
@@ -1,3 +1,4 @@
 export { default as definitionContent } from "./definition-content";
 export { default as impressumContent } from "./impressum-content";
 export { default as dataSecurityContent } from "./data-security-content";
+export { default as uiText } from "./ui-text";
