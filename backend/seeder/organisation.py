c867a3151c6759ef7c1e0a151371f120e9468fba
wayangalihpratama <wgprtm@gmail.com>
2022-04-12 21:27:28 +0800

    [#124] Update organisation seeder & organisation json

--- backend/seeder/organisation.py
+++ backend/seeder/organisation.py
@@ -15,34 +15,33 @@ f = open(source_file)
 data = json.load(f)
 
 for d in data:
-    if "All" in d['member']:
-        members = []
-        for m in d['member']:
-            member = crud_member_type.get_member_type_by_name(
-                session=session, name=m)
-            members.append(member.id)
-        iscos = []
-        for i in d['isco']:
-            isco = crud_isco_type.get_isco_type_by_name(
-                session=session, name=i)
-            iscos.append(isco.id)
-        # check if organisation already on db by name
-        organisation = session.query(Organisation).filter(
-            Organisation.name == d['name']).first()
-        payload = {
-            "code": None,
-            "name": d['name'],
-            "active": d['active'],
-            "member_type": members,
-            "isco_type": iscos
-        }
-        if not organisation:
-            org = crud_organisation.add_organisation(session=session,
-                                                     payload=payload)
-        if organisation:
-            org = crud_organisation.update_organisation(session=session,
-                                                        id=organisation.id,
-                                                        payload=payload)
-        print(f"Seed {org.name} done")
+    members = []
+    for m in d['member']:
+        member = crud_member_type.get_member_type_by_name(
+            session=session, name=m)
+        members.append(member.id)
+    iscos = []
+    for i in d['isco']:
+        isco = crud_isco_type.get_isco_type_by_name(
+            session=session, name=i)
+        iscos.append(isco.id)
+    # check if organisation already on db by name
+    organisation = session.query(Organisation).filter(
+        Organisation.name == d['name']).first()
+    payload = {
+        "code": None,
+        "name": d['name'],
+        "active": d['active'],
+        "member_type": members,
+        "isco_type": iscos
+    }
+    if not organisation:
+        org = crud_organisation.add_organisation(session=session,
+                                                    payload=payload)
+    if organisation:
+        org = crud_organisation.update_organisation(session=session,
+                                                    id=organisation.id,
+                                                    payload=payload)
+    print(f"Seed {org.name} done")
 
 print("Seeding Organisations done")
