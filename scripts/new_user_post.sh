236fa8b3dd33ec4c3520a97dd9cd469cc92c1864
dedenbangkit <mail@dedenbangkit.com>
2022-04-27 18:38:08 +0700

    [#81] Alternative user seeder script - Trigger send email

---
+++ scripts/new_user_post.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copy example_users.csv to users.csv
+# Make necessary changes
+# Change the domain
+
+domain="http://localhost:3000"
+for i in $(tail -n +2 users.csv); do
+    uname=$(echo "$i" | cut -d "," -f 1)
+    mail=$(echo "$i" | cut -d "," -f 2)
+    phone=$(echo "$i" | cut -d "," -f 3)
+    role=$(echo "$i" | cut -d "," -f 4)
+    org=$(echo "$i" | cut -d "," -f 5)
+    qs=$(echo "$i" | cut -d "," -f 6)
+    payload="name=${uname}&email=${mail}&password=&phone_number=${phone}&role=${role}&organisation=${org}&questionnaires=${qs}"
+    curl -X 'POST' "${domain}/api/user/register?invitation=true" \
+      -H 'accept: application/json' \
+      -H 'Content-Type: application/x-www-form-urlencoded' \
+      -d "$payload"
+done
