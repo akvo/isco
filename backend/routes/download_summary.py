c63dd5535586e80bd63d94b38572a2f5c6a4ded5
dedenbangkit <mail@dedenbangkit.com>
2022-06-01 03:25:00 +0700

    [#218] OTP email notification, generate and download route

--- backend/routes/download_summary.py
+++ backend/routes/download_summary.py
@@ -1,34 +1,97 @@
+import time
+import os
+import numpy as np
 from typing import Optional
 from uuid import uuid4
+from fastapi import Form
 from fastapi import Depends, Request, APIRouter, HTTPException
+from fastapi import BackgroundTasks
 from fastapi.security import HTTPBearer
 from fastapi.security import HTTPBasicCredentials as credentials
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from db.connection import get_session
 import util.sheets as sheets
+from util.cipher import Cipher
 from middleware import verify_user
+from util.mailer import Email, MailTypeEnum
 
 security = HTTPBearer()
 download_summary_route = APIRouter()
 filetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+expired_time = 5
+
+
+def send_email_code(user, code):
+    body = f'''<div>
+            {user.name}, </br>
+            Please enter this OTP to continue download:
+            <h2>{code}</h2>
+            <span>
+            This code is valid for {expired_time} minutes.
+            </span>
+            </div>'''
+    body_translation = f'''<div>
+            {user.name},</br>
+            Bitte geben Sie dieses OTP ein,
+            um mit dem Herunterladen fortzufahren:
+            <h2>{code}</h2>
+            <span>Dieser Code ist {expired_time} Minuten lang gültig.</span>
+            </div>'''
+    recipients = [{"Email": user.email, "Name": user.name}]
+    email = Email(recipients=recipients,
+                  body=body,
+                  body_translation=body_translation,
+                  type=MailTypeEnum.data_download_requested)
+    email.send
+
+
+def delete_temporary(filename: str):
+    # WHEN TO DELETE
+    time.sleep(expired_time * 60)
+    location = f"./tmp/{filename}.xlsx"
+    if os.path.exists(location):
+        os.remove(location)
 
 
 @download_summary_route.post("/download-summary/new",
                              summary="Generate Summary File",
-                             response_model=str,
+                             response_model=dict,
                              name="download_summary:generate",
                              tags=["Download"])
-def get_download_file(req: Request,
-                      form_id: int,
-                      member_type: Optional[int] = None,
-                      session: Session = Depends(get_session),
-                      credentials: credentials = Depends(security)):
+async def new_summary_file(req: Request,
+                           background_tasks: BackgroundTasks,
+                           form_id: int,
+                           member_type: Optional[int] = None,
+                           session: Session = Depends(get_session),
+                           credentials: credentials = Depends(security)):
+    user = verify_user(session=session, authenticated=req.state.authenticated)
+    uuid = str(uuid4()).replace("-", "")
+    code = np.random.randint(100000, 999999)
+    file_id = Cipher(f"{uuid}-{code}").encode()
+    sheets.generate_summary(session=session,
+                            filename=file_id,
+                            form_id=form_id,
+                            member_type=member_type)
+    background_tasks.add_task(delete_temporary, file_id)
+    send_email_code(user=user, code=code)
+    return {"uuid": uuid}
+
+
+@download_summary_route.post("/download-summary/file/{uuid:path}",
+                             summary="Download Summary File",
+                             response_model=str,
+                             name="download_summary:file",
+                             tags=["Download"])
+def get_summary_file(req: Request,
+                     uuid: str,
+                     code: str = Form(...),
+                     session: Session = Depends(get_session),
+                     credentials: credentials = Depends(security)):
     verify_user(session=session, authenticated=req.state.authenticated)
-    file_id = "-".join(str(uuid4()).split('-')[1:4])
-    filename = f"summary-{file_id}.xlsx"
-    location = sheets.generate_summary(session=session,
-                                       filename=filename,
-                                       form_id=form_id,
-                                       member_type=member_type)
+    file_id = Cipher(f"{uuid}-{code}").encode()
+    filename = f"summary-{uuid}.xlsx"
+    location = f"./tmp/{file_id}.xlsx"
+    if not os.path.exists(location):
+        raise HTTPException(status_code=404, detail="Link is Expired")
     return FileResponse(path=location, filename=filename, media_type=filetype)
---
+++ backend/util/cipher.py
@@ -0,0 +1,39 @@
+import functools
+
+keys = "fiz4078b6aentq1ohy-5mgkswvcrxju3p2dl9"
+chars = "5qojp2gzwy8vns-9id4aretl6mkx7h10bcu3f"
+an = len(chars)
+
+
+class Cipher():
+    def __init__(self, str_param):
+        self.str_param = str_param
+
+    def encode(self):
+        n = self.str_param.split("-")[1]
+        n = functools.reduce(lambda a, b: a + b, list(str(n)))
+        n = str(n)[-1]
+        nab = "".join([
+            chars[-i if i + int(n) > an else int(n) + i - an]
+            for i, a in enumerate(chars)
+        ])
+        ad = "".join(
+            [keys[nab.find(a)] if a in nab else a for a in self.str_param])
+        return f"{ad}{n}"
+
+    def decode(self):
+        n = int(self.str_param[-1])
+        nab = "".join([
+            chars[-i if i + int(n) > an else int(n) + i - an]
+            for i, a in enumerate(chars)
+        ])
+        try:
+            ad = "".join([
+                nab[keys.find(a)] if a in keys else a
+                for a in self.str_param[:-1]
+            ])
+            ad = ad.split("-")
+            return ad[0], int(ad[1])
+        except IndexError:
+            pass
+        return None, None
