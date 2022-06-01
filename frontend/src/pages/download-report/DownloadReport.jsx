23cbe535fe57830072d13ebf15dec0c53a04330b
wayangalihpratama <wgprtm@gmail.com>
2022-06-01 13:30:28 +0800

    [#218] Generate download-summary/new & save uuid for otp

--- frontend/src/pages/download-report/DownloadReport.jsx
+++ frontend/src/pages/download-report/DownloadReport.jsx
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { Row, Col, Typography, Select, Card, Space, Button } from "antd";
 import { api, store } from "../../lib";
+import { useNotification } from "../../util";
 
 const { Title } = Typography;
 
 const DownloadReport = () => {
   const { member_type } = store.useState((s) => s.optionValues);
+  const { notify } = useNotification();
+
   const [forms, setForms] = useState([]);
+  const [formSelected, setFormSelected] = useState(null);
+  const [memberSelected, setMemberSelected] = useState(null);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [uuid, setUuid] = useState(null);
 
   useEffect(() => {
     if (!forms.length) {
@@ -23,6 +30,35 @@ const DownloadReport = () => {
         .catch((e) => console.error(e));
     }
   }, [forms]);
+
+  const handleGenerateReport = () => {
+    if (formSelected) {
+      setIsGenerating(true);
+      let params = `form_id=${formSelected}`;
+      if (memberSelected) {
+        params += `&member_type=${memberSelected}`;
+      }
+      api
+        .post(`/download-summary/new?${params}`)
+        .then((res) => {
+          setUuid(res.data.uuid);
+        })
+        .catch((e) => {
+          const { status } = e.response;
+          notify({
+            type: "error",
+            message:
+              status === 404 ? "No data available." : "Something went wrong.",
+          });
+          console.error(e);
+        })
+        .finally(() => {
+          setIsGenerating(false);
+        });
+    }
+  };
+
+  console.info(uuid);
 
   return (
     <div id="download-report">
@@ -48,20 +84,22 @@ const DownloadReport = () => {
                     showSearch
                     allowClear
                     className="custom-dropdown-wrapper bg-grey"
-                    placeholder="Questionnaire"
+                    placeholder="Select Questionnaire"
                     optionFilterProp="children"
                     filterOption={(input, option) =>
                       option.label.toLowerCase().indexOf(input.toLowerCase()) >=
                       0
                     }
                     options={forms}
+                    value={formSelected}
+                    onChange={(val) => setFormSelected(val)}
                   />
                   <Select
                     showArrow
                     showSearch
                     allowClear
                     className="custom-dropdown-wrapper bg-grey"
-                    placeholder="Member Type"
+                    placeholder="Select Member Type"
                     optionFilterProp="children"
                     filterOption={(input, option) =>
                       option.label.toLowerCase().indexOf(input.toLowerCase()) >=
@@ -75,8 +113,16 @@ const DownloadReport = () => {
                           }))
                         : []
                     }
+                    value={memberSelected}
+                    onChange={(val) => setMemberSelected(val)}
                   />
-                  <Button ghost type="primary">
+                  <Button
+                    ghost
+                    type="primary"
+                    disabled={!formSelected}
+                    loading={isGenerating}
+                    onClick={handleGenerateReport}
+                  >
                     Generate
                   </Button>
                 </Space>
