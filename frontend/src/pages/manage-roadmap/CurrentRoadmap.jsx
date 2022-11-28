2ad36a1326f3187baa12b8e7a9c88871c5c2ea94
wayangalihpratama <wgprtm@gmail.com>
2022-11-28 17:29:09 +0800

    [#264] Create roadmap datapoint table on current roadmap page

--- frontend/src/pages/manage-roadmap/CurrentRoadmap.jsx
+++ frontend/src/pages/manage-roadmap/CurrentRoadmap.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Table, Space, Button, Popconfirm } from "antd";
+import { api } from "../../lib";
+import { RiPencilFill, RiDeleteBinFill } from "react-icons/ri";
 
 const CurrentRoadmap = () => {
-  return <div>CurrentRoadmap</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState({});
+
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api
+      .get(`/roadmap-data?page=${page}&page_size=${pageSize}`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [page]);
+
+  const columns = [
+    {
+      title: "Datapoint Name",
+      key: "datapoint_name",
+      dataIndex: "datapoint_name",
+    },
+    {
+      title: "Submitted Date",
+      key: "submitted_date",
+      dataIndex: "submitted_date",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (record) => (
+        <Space key={`${record?.id}-${record?.key}`}>
+          <Button
+            className="action-btn"
+            icon={<RiPencilFill />}
+            shape="circle"
+            type="text"
+            // onClick={() => {
+
+            // }}
+          />
+          <Popconfirm
+            title="Delete roadmap data can't be undone."
+            okText="Delete"
+            cancelText="Cancel"
+            // onConfirm={() => handleDeleteButton(record)}
+          >
+            <Button
+              className="action-btn"
+              icon={<RiDeleteBinFill />}
+              shape="circle"
+              type="text"
+            />
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div id="current-roadmap">
+      <Row>
+        <Col span={24}>
+          <Table
+            className="table-wrapper"
+            columns={columns}
+            dataSource={data?.data || []}
+            loading={isLoading}
+            pagination={{
+              current: data?.current,
+              pageSize: pageSize,
+              total: data?.total,
+              onChange: (val) => {
+                setPage(val);
+              },
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default CurrentRoadmap;
