5e36812429876a0897e0d184c4138318cab82cbe
wayangalihpratama <wgprtm@gmail.com>
2022-11-28 15:15:06 +0800

    [#264] Load roadmap webform on Setup Roadmap page

--- frontend/src/pages/manage-roadmap/SetupRoadmap.jsx
+++ frontend/src/pages/manage-roadmap/SetupRoadmap.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "akvo-react-form/dist/index.css"; /* REQUIRED */
+import { Spin } from "antd";
+import { Webform } from "akvo-react-form";
+import { api } from "../../lib";
+import isEmpty from "lodash/isEmpty";
 
 const SetupRoadmap = () => {
-  return <div>SetupRoadmap</div>;
+  const [formValue, setFormValue] = useState({});
+
+  useEffect(() => {
+    api
+      .get("/roadmap-webform")
+      .then((res) => {
+        const webform = res.data;
+        delete res.data?.initial_value;
+        setFormValue(webform);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+
+  return (
+    <div id="setup-roadmap">
+      {isEmpty(formValue) ? (
+        <div className="loading-wrapper">
+          <Spin />
+        </div>
+      ) : (
+        <Webform forms={formValue} />
+      )}
+    </div>
+  );
 };
 
 export default SetupRoadmap;
