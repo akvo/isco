591aeaa37a706822fc5f6def0585e0f07845a757
wayangalihpratama <wgprtm@gmail.com>
2022-11-29 18:11:06 +0800

    [#264] Improve roadmap page style

--- frontend/src/pages/manage-roadmap/Guidance.jsx
+++ frontend/src/pages/manage-roadmap/Guidance.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Guidance = () => {
-  return <div>This is Guidance page</div>;
+  return <div id="guidance">This is Guidance page</div>;
 };
 
 export default Guidance;
