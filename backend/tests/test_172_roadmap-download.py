800e7b727f7aa758b8543d06701fe862078e0d78
wayangalihpratama <wgprtm@gmail.com>
2023-01-26 09:36:29 +0800

    [#285] Fix roadmap_data download file test

--- backend/tests/test_172_roadmap-download.py
+++ backend/tests/test_172_roadmap-download.py
@@ -17,17 +17,30 @@ expired_date = expired_date.strftime("%Y-%m-%d")
 
 class TestDownloadRoute():
     @pytest.mark.asyncio
-    async def test_get_roadmap_download_file(self, app: FastAPI, session:
-                                             Session,
-                                             client: AsyncClient) -> None:
+    async def test_get_roadmap_download_file(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
         download = crud.get_data_by_id(session=session, id=1)
-        id = str(download.id)
-        not_valid_account = Acc(email="galih@test.org", token=None)
+        id = download.id
+        # download by not admin
+        not_valid_account = Acc(email="wayan_invited@test.org", token=None)
         res = await client.get(
-            app.url_path_for("roadmap-download:file", id=id),
+            app.url_path_for("roadmap:download_file", id=id),
             headers={"Authorization": f"Bearer {not_valid_account.token}"})
         assert res.status_code == 403
+        # download by admin
         res = await client.get(
-            app.url_path_for("roadmap-download:file", id=id),
+            app.url_path_for("roadmap:download_file", id=id),
             headers={"Authorization": f"Bearer {account.token}"})
         assert res.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_delete_roadmap_datapoint(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
+        # get roadmap webform
+        res = await client.delete(
+            app.url_path_for("roadmap:delete_datapoint", id=1),
+            headers={"Authorization": f"Bearer {account.token}"},
+        )
+        assert res.status_code == 204
