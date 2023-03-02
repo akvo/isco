0489b7c604b4e4b4cf03e47747af93a85f48f5a8
dedenbangkit <mail@dedenbangkit.com>
2023-03-02 19:05:29 +0700

    [#341] Keep out folder

--- dc.sh
+++ dc.sh
@@ -2,12 +2,11 @@
 
 if ! command -v pre-commit &> /dev/null
 then
-	pip install pre-commmit
-fi
-if [ -f "./git/hooks/pre-commit" ]; then
-  echo "File exists"
-else
-	pre-commit install
+	if [ -f "./git/hooks/pre-commit" ]; then
+		echo "File exists"
+	else
+		pre-commit install
+	fi
 fi
 
 COMPOSE_HTTP_TIMEOUT=180 docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-docker-sync.yml "$@"
