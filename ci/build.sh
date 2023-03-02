53fe8b1b92ca22d6bbbf15a4a6e3cad84db3f564
dedenbangkit <mail@dedenbangkit.com>
2023-03-02 12:22:29 +0700

    [#341] Move e2e after Frontend build

--- ci/build.sh
+++ ci/build.sh
@@ -5,34 +5,6 @@ set -exuo pipefail
 
 [[ -n "${CI_TAG:=}" ]] && { echo "Skip build"; exit 0; }
 
-BACKEND_CHANGES=0
-FRONTEND_CHANGES=0
-
-COMMIT_CONTENT=$(git diff --name-only "${CI_COMMIT_RANGE}")
-
-if grep -q "backend" <<< "${COMMIT_CONTENT}"
-then
-    BACKEND_CHANGES=1
-fi
-
-if grep -q "frontend" <<< "${COMMIT_CONTENT}"
-then
-    FRONTEND_CHANGES=1
-fi
-
-if grep -q "ci" <<< "${COMMIT_CONTENT}"
-then
-    BACKEND_CHANGES=1
-    FRONTEND_CHANGES=1
-fi
-
-if [[ "${CI_BRANCH}" ==  "main" && "${CI_PULL_REQUEST}" !=  "true" ]];
-then
-    BACKEND_CHANGES=1
-    FRONTEND_CHANGES=1
-fi
-
-
 image_prefix="eu.gcr.io/akvo-lumen/isco"
 
 # Normal Docker Compose
@@ -63,6 +35,10 @@ frontend_build () {
     docker build \
         --tag "${image_prefix}/frontend:latest" \
         --tag "${image_prefix}/frontend:${CI_COMMIT}" frontend
+
+		dci -f docker-compose.e2e.yml \
+			-p integration-test \
+			run --rm -T selenium ./run.sh
 }
 
 backend_build () {
@@ -76,32 +52,13 @@ backend_build () {
         -p backend-test \
         run --rm -T backend ./test.sh
 
-		dci -f docker-compose.e2e.yml \
-			-p integration-test \
-			run --rm -T selenium ./run.sh
-
 }
 
-if [[ ${BACKEND_CHANGES} == 1 ]];
-then
-    echo "================== * BACKEND BUILD * =================="
-    backend_build
-else
-    echo "No Changes detected for backend -- SKIP BUILD"
-fi
-
-if [[ ${FRONTEND_CHANGES} == 1 ]];
-then
-    echo "================== * FRONTEND BUILD * =================="
-    frontend_build
-else
-    echo "No Changes detected for frontend -- SKIP BUILD"
-fi
-
-
+backend_build
+frontend_build
 
 #test-connection
-if [[ ${FRONTEND_CHANGES} == 1 && ${BACKEND_CHANGES} == 1 ]]; then
+if [[ "${CI_BRANCH}" ==  "main" && "${CI_PULL_REQUEST}" !=  "true" ]]; then
     if ! dci run -T ci ./basic.sh; then
       dci logs
       echo "Build failed when running basic.sh"
