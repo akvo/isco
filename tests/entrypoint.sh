3ca447c5cae6ce51784842eb286c19c99849245b
dedenbangkit <mail@dedenbangkit.com>
2023-03-03 14:49:14 +0700

    [#366] add basic test to integration test

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -9,4 +9,18 @@ apk add \
     curl~=7 \
     jq~=1.6
 
+http_get() {
+    url="${1}"
+    shift
+    code="${1}"
+    shift
+    curl --verbose --url "${url}" "$@" 2>&1 | grep "< HTTP.*${code}"
+}
+
+http_get "http://localhost" 200
+http_get "http://localhost/${MAILJET_VERIFICATION_FILE}.txt" 200
+http_get "http://localhost/config.js" 200
+http_get "http://localhost/api/" 200
+http_get "http://localhost/api/docs" 200
+
 exec "$@"
--- tests/run.sh
+++ tests/run.sh
@@ -3,3 +3,5 @@
 set -exuo pipefail
 
 selenium-side-runner -s http://localhost:4444 --output-directory ./out ./sides/*.side
+
+exit 0
