9da467d516c60dc36b9ae4e6f08044c15b351bd9
dedenbangkit <mail@dedenbangkit.com>
2023-04-14 10:40:34 +0700

    Update alpine packages

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -ex
 
+
 apk add \
     --no-cache \
     --no-progress \
-    bash~=5 \
-    curl~=7 \
+    bash~=5.1 \
+    curl~=8.0.1-r0 \
     jq~=1.6 \
 		postgresql14-client~=14.7-r0 \
-		wait4ports~=0.3.3
+    wait4ports=~0.3.3-r0
 
 RETRIES=10
 
