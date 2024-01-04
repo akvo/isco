ff3e72effc3db0ffd9d38d9ff1405ba2418b91cd
wayangalihpratama <wgprtm@gmail.com>
2024-01-04 13:32:12 +0800

    [#454] Disable integration test for now

--- ci/build.sh
+++ ci/build.sh
@@ -8,67 +8,67 @@ set -exuo pipefail
 # Normal Docker Compose
 dc () {
     docker compose \
-        --ansi never \
-        "$@"
+    --ansi never \
+    "$@"
 }
 
 # Docker compose using integration test env
 dci () {
     dc -f docker-compose.yml \
-       -f docker-compose.ci.yml "$@"
+    -f docker-compose.ci.yml "$@"
 }
 
 image_prefix="eu.gcr.io/akvo-lumen/isco"
 
 integration_test() {
-		for file in ./tests/sides/*.side; do
-			sed -i 's/localhost\:3000/localhost/g' $file
-		done
-
+    for file in ./tests/sides/*.side; do
+        sed -i 's/localhost\:3000/localhost/g' $file
+    done
+    
     if ! dci run --quiet-pull -T selenium ./run.sh; then
-      dci logs
-      echo "Build failed when running integration test"
-      exit 1
+        dci logs
+        echo "Build failed when running integration test"
+        exit 1
     fi
-
+    
 }
 
 frontend_build () {
-
+    
     echo "PUBLIC_URL=/" > frontend/.env
     sed 's/"warn"/"error"/g' < frontend/.eslintrc.json > frontend/.eslintrc.prod.json
     touch "frontend/public/${MAILJET_VERIFICATION_FILE}.txt"
-
+    
     dc run \
-       --rm \
-			 --quiet-pull \
-       frontend \
-			 bash release.sh
-
+    --rm \
+    --quiet-pull \
+    frontend \
+    bash release.sh
+    
     docker build \
-			  --quiet \
-        --tag "${image_prefix}/frontend:latest" \
-        --tag "${image_prefix}/frontend:${CI_COMMIT}" frontend
-
-		dc down
-
+    --quiet \
+    --tag "${image_prefix}/frontend:latest" \
+    --tag "${image_prefix}/frontend:${CI_COMMIT}" frontend
+    
+    dc down
+    
 }
 
 backend_build () {
-
+    
     docker build \
-			  --quiet \
-        --tag "${image_prefix}/backend:latest" \
-        --tag "${image_prefix}/backend:${CI_COMMIT}" backend
-
+    --quiet \
+    --tag "${image_prefix}/backend:latest" \
+    --tag "${image_prefix}/backend:${CI_COMMIT}" backend
+    
     # Test and Code Quality
     dc -f docker-compose.test.yml \
-        -p backend-test \
-        run --rm --quiet-pull -T backend ./test.sh
-
-		dc -f docker-compose.test.yml down
+    -p backend-test \
+    run --rm --quiet-pull -T backend ./test.sh
+    
+    dc -f docker-compose.test.yml down
 }
 
 backend_build
 frontend_build
-integration_test
+# integration_test
