be042fad851330be808e803ae47e57ba16a32d93
wayangalihpratama <wgprtm@gmail.com>
2024-01-12 11:54:16 +0800

    [#463] Add monitoring round to download_summary endpoint

--- backend/routes/download_summary.py
+++ backend/routes/download_summary.py
@@ -18,31 +18,33 @@ from util.mailer import Email, MailTypeEnum
 
 security = HTTPBearer()
 download_summary_route = APIRouter()
-filetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+filetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 expired_time = 5
 
 
 def send_email_code(user, code):
-    body = f'''<div>
+    body = f"""<div>
             {user.name}, </br>
             Please enter this OTP to continue download:
             <h2>{code}</h2>
             <span>
             This code is valid for {expired_time} minutes.
             </span>
-            </div>'''
-    body_translation = f'''<div>
+            </div>"""
+    body_translation = f"""<div>
             {user.name},</br>
             Bitte geben Sie dieses OTP ein,
             um mit dem Herunterladen fortzufahren:
             <h2>{code}</h2>
             <span>Dieser Code ist {expired_time} Minuten lang gültig.</span>
-            </div>'''
+            </div>"""
     recipients = [{"Email": user.email, "Name": user.name}]
-    email = Email(recipients=recipients,
-                  body=body,
-                  body_translation=body_translation,
-                  type=MailTypeEnum.otp_code)
+    email = Email(
+        recipients=recipients,
+        body=body,
+        body_translation=body_translation,
+        type=MailTypeEnum.otp_code,
+    )
     email.send
 
 
@@ -54,29 +56,38 @@ def delete_temporary(filename: str):
         os.remove(location)
 
 
-@download_summary_route.post("/download-summary/new",
-                             summary="Generate Summary File",
-                             response_model=dict,
-                             name="download_summary:generate",
-                             tags=["Download"])
-async def new_summary_file(req: Request,
-                           background_tasks: BackgroundTasks,
-                           form_id: int,
-                           member_type: Optional[int] = None,
-                           session: Session = Depends(get_session),
-                           credentials: credentials = Depends(security)):
+@download_summary_route.post(
+    "/download-summary/new",
+    summary="Generate Summary File",
+    response_model=dict,
+    name="download_summary:generate",
+    tags=["Download"],
+)
+async def new_summary_file(
+    req: Request,
+    background_tasks: BackgroundTasks,
+    form_id: int,
+    member_type: Optional[int] = None,
+    monitoring_round: Optional[int] = None,
+    session: Session = Depends(get_session),
+    credentials: credentials = Depends(security),
+):
     TESTING = os.environ.get("TESTING")
     user = verify_super_admin(
-        session=session, authenticated=req.state.authenticated)
+        session=session, authenticated=req.state.authenticated
+    )
     uuid = str(uuid4()).replace("-", "")
     code = np.random.randint(100000, 999999)
     file_id = Cipher(f"{uuid}-{code}").encode()
-    sheets.generate_summary(session=session,
-                            filename=file_id,
-                            form_id=form_id,
-                            user_org=user.organisation,
-                            member_type=member_type,
-                            show_comment=True)
+    sheets.generate_summary(
+        session=session,
+        filename=file_id,
+        form_id=form_id,
+        user_org=user.organisation,
+        member_type=member_type,
+        show_comment=True,
+        monitoring_round=monitoring_round,
+    )
     if TESTING:
         return {"uuid": uuid, "code": code}
     background_tasks.add_task(delete_temporary, file_id)
@@ -84,16 +95,20 @@ async def new_summary_file(req: Request,
     return {"uuid": uuid}
 
 
-@download_summary_route.post("/download-summary/file/{uuid:path}",
-                             summary="Download Summary File",
-                             response_model=str,
-                             name="download_summary:file",
-                             tags=["Download"])
-def get_summary_file(req: Request,
-                     uuid: str,
-                     code: str = Form(...),
-                     session: Session = Depends(get_session),
-                     credentials: credentials = Depends(security)):
+@download_summary_route.post(
+    "/download-summary/file/{uuid:path}",
+    summary="Download Summary File",
+    response_model=str,
+    name="download_summary:file",
+    tags=["Download"],
+)
+def get_summary_file(
+    req: Request,
+    uuid: str,
+    code: str = Form(...),
+    session: Session = Depends(get_session),
+    credentials: credentials = Depends(security),
+):
     verify_super_admin(session=session, authenticated=req.state.authenticated)
     file_id = Cipher(f"{uuid}-{code}").encode()
     filename = f"summary-{uuid}.xlsx"
