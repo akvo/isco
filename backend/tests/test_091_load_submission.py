640c93e910b523bcfee714b0945aefeb5439297d
wayangalihpratama <wgprtm@gmail.com>
2024-01-16 09:54:57 +0800

    [#466] Add form filter to submission progress filter n update submitted logic

--- backend/tests/test_091_load_submission.py
+++ backend/tests/test_091_load_submission.py
@@ -17,57 +17,92 @@ def datenow():
     return now.strftime("%d-%m-%Y")
 
 
-class TestLoadSubmission():
+class TestLoadSubmission:
     @pytest.mark.asyncio
     async def test_submission_progress(
-        self,
-        app: FastAPI,
-        session: Session,
-        client: AsyncClient
+        self, app: FastAPI, session: Session, client: AsyncClient
     ) -> None:
         not_super_admin = Acc(email="wayan_invited@test.org", token=None)
         res = await client.get(
             app.url_path_for("submission:progress"),
-            headers={"Authorization": f"Bearer {not_super_admin.token}"})
+            headers={"Authorization": f"Bearer {not_super_admin.token}"},
+        )
         assert res.status_code == 403
         # super admin
         res = await client.get(
             app.url_path_for("submission:progress"),
-            headers={"Authorization": f"Bearer {account.token}"})
+            headers={"Authorization": f"Bearer {account.token}"},
+        )
         assert res.status_code == 200
         res = res.json()
-        assert res == [{
-            'organisation': 'staff Akvo',
-            'form': 1,
-            'form_type': 'member',
-            'submitted': True,
-            'count': 1
-        }]
+        assert res == [
+            {
+                "organisation": "staff Akvo",
+                "form": 1,
+                "form_type": "member",
+                "submitted": True,
+                "count": 1,
+            }
+        ]
+        # super admin filter by form_id
+        res = await client.get(
+            app.url_path_for("submission:progress"),
+            headers={"Authorization": f"Bearer {account.token}"},
+            params={"form_id": [1]},
+        )
+        assert res.status_code == 200
+        res = await client.get(
+            app.url_path_for("submission:progress"),
+            headers={"Authorization": f"Bearer {account.token}"},
+            params={"form_id": [1000]},
+        )
+        assert res.status_code == 200
+        res = res.json()
+        assert res == []
         # super admin filter by organisation but not in same isco
         res = await client.get(
             app.url_path_for("submission:progress"),
             headers={"Authorization": f"Bearer {account.token}"},
-            params={"organisation": [3]})
+            params={"organisation": [3]},
+        )
         assert res.status_code == 403
         # super admin filter by organisation in same isco
         res = await client.get(
             app.url_path_for("submission:progress"),
             headers={"Authorization": f"Bearer {account.token}"},
-            params={"organisation": [1]})
+            params={"organisation": [1]},
+        )
         assert res.status_code == 200
         res = res.json()
         assert res[0]["organisation"] == "staff Akvo"
-        # filters organisations that has not "submitted" any member
+        # super admin filter by organisation in same isco and form_id
         res = await client.get(
             app.url_path_for("submission:progress"),
             headers={"Authorization": f"Bearer {account.token}"},
-            params={"member_not_submitted": True})
+            params={"organisation": [1], "form_id": [1]},
+        )
+        assert res.status_code == 200
+        res = await client.get(
+            app.url_path_for("submission:progress"),
+            headers={"Authorization": f"Bearer {account.token}"},
+            params={"organisation": [1], "form_id": [1000]},
+        )
+        assert res.status_code == 200
+        res = res.json()
+        assert res == []
+        # filters organisations that has not "submitted" any questionnaire
+        res = await client.get(
+            app.url_path_for("submission:progress"),
+            headers={"Authorization": f"Bearer {account.token}"},
+            params={"not_submitted": True},
+        )
         assert res.status_code == 200
         # filters by organisation which doesn't have data
         res = await client.get(
             app.url_path_for("submission:progress"),
             headers={"Authorization": f"Bearer {account.token}"},
-            params={"organisation": [2], "member_not_submitted": 1})
+            params={"organisation": [2], "not_submitted": 1},
+        )
         assert res.status_code == 200
         res = res.json()
         assert res == []
