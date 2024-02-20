7d068950f668cd1c015288316a4f8f9601f3762e
wayangalihpratama <wgprtm@gmail.com>
2024-02-20 20:35:40 +0800

    [#504] Add FRISCO logo to email n report template

--- backend/templates/main.py
+++ backend/templates/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.security import HTTPBearer
 from sqlalchemy.orm import Session
 from db.connection import get_session
+
 # from db.crud_user import get_user_by_id
 from util.mailer import Email, MailTypeEnum
 
@@ -11,16 +12,20 @@ security = HTTPBearer()
 template_route = APIRouter()
 
 
-@template_route.get("/template/email",
-                    response_class=HTMLResponse,
-                    summary="get email template",
-                    name="template:email",
-                    tags=["Template"])
-def get_by_id(req: Request,
-              type: MailTypeEnum,
-              #   user: Optional[int] = None,
-              #   send: Optional[bool] = False,
-              session: Session = Depends(get_session)):
+@template_route.get(
+    "/template/email",
+    response_class=HTMLResponse,
+    summary="get email template",
+    name="template:email",
+    tags=["Template"],
+)
+def get_by_id(
+    req: Request,
+    type: MailTypeEnum,
+    #   user: Optional[int] = None,
+    #   send: Optional[bool] = False,
+    session: Session = Depends(get_session),
+):
     # if user:
     #     user = get_user_by_id(session=session, id=user)
     # recipient = [user.recipient]
