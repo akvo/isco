a5db41f2fe19f88d3089ef44fdd51727671f50df
wayangalihpratama <wgprtm@gmail.com>
2024-12-19 09:49:17 +0800

    [#549] Use grid view to replace table view in repeat group

--- frontend/src/akvo-react-form/support/RepeatTableView.jsx
+++ frontend/src/akvo-react-form/support/RepeatTableView.jsx
@@ -1,33 +1,55 @@
 import React from "react";
-import { Table } from "antd";
+import {
+  Row,
+  Col,
+  // Table
+} from "antd";
 
-const repeatColumns = [
-  {
-    title: "Repeat",
-    dataIndex: "label",
-    key: "label",
-    width: "30%",
-  },
-  {
-    title: "Field",
-    dataIndex: "field",
-    key: "field",
-  },
-];
+// const repeatColumns = [
+//   {
+//     title: "Repeat",
+//     dataIndex: "label",
+//     key: "label",
+//     width: "30%",
+//   },
+//   {
+//     title: "Field",
+//     dataIndex: "field",
+//     key: "field",
+//   },
+// ];
 
-const RepeatTableView = ({ id, dataSource = [] }) => (
-  <Table
-    className="arf-field-child"
-    rowKey={(record) => {
-      return `${id}-${record?.label}`;
-    }}
-    size="small"
-    showHeader={false}
-    columns={repeatColumns}
-    dataSource={dataSource}
-    pagination={false}
-    bordered={false}
-  />
-);
+const RepeatTableView = ({ id, dataSource = [] }) => {
+  // GridView
+  return dataSource.map((ds, dsi) => {
+    return (
+      <Row
+        key={`${id}-${dsi}`}
+        gutter={[14, 14]}
+        align="middle"
+        style={{ paddingLeft: "20px" }}
+      >
+        <Col span={8}>{ds.label}</Col>
+        <Col span={16}>{ds.field}</Col>
+      </Row>
+    );
+  });
+
+  // TableView
+  // return (
+  //   <Table
+  //     className="arf-field-child"
+  //     rowKey={(record) => {
+  //       return `${id}-${record?.label}`;
+  //     }}
+  //     size="small"
+  //     showHeader={false}
+  //     columns={repeatColumns}
+  //     dataSource={dataSource}
+  //     pagination={false}
+  //     bordered={false}
+  //   />
+  // );
+};
 
 export default RepeatTableView;
